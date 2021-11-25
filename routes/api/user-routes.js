b5926defd25a4c56fb89d922ca0f5b87071040c3
Makda Girmay <makdagirmay4@gmail.com>
2021-11-25 17:08:28 -0500

    excluding passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,31 +3,34 @@ const { User } = require('../../models');
 const bcrypt = require('bcrypt');
 // GET /api/users
 router.get('/', (req, res) => {
-    //Access our User moder and run .findAll() method)
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+      })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+          console.log(err);
+          res.status(500).json(err);
         });
 });
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
-            id: req.params.id
+          id: req.params.id
         }
-    })
+      })
         .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+          }
+          res.json(dbUserData);
         })
         .catch(err => {
-            console.log(err);
+          console.log(err);
+          res.status(500).json(err);
         });
 });
 
